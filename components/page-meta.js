223c75cc0203acd6919efa63fee838a84fa41792
Nirmal <contact@appdews.com>
2021-08-21 21:14:19 +0530

    updated items

--- components/page-meta.js
+++ components/page-meta.js
@@ -6,7 +6,10 @@ const MetaTags = () => {
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"></meta>
 			<link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400&display=swap" rel="stylesheet" />
-
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+    <link rel="manifest" href="/favicons/site.webmanifest" />
       <meta
         name="apple-mobile-web-app-status-bar-style"
         content="black-translucent"
